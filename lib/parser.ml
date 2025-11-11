5e4e6496e5c8d2e182eccb4e53f4bf936b757810
Ľubomír Kurčák <29227697+lkurcak@users.noreply.github.com>
2025-11-11 02:24:04 +0100

    fix operation order

--- lib/parser.ml
+++ lib/parser.ml
@@ -14,29 +14,33 @@ let parse_expr str =
   let state = { tokens; pos = 0 } in
 
   let rec parse_add () =
-    let left = parse_mul () in
-    match current state with
-    | Some PLUS ->
-        advance state;
-        let right = parse_add () in
-        Expr.Add (left, right)
-    | Some MINUS ->
-        advance state;
-        let right = parse_add () in
-        Expr.Sub (left, right)
-    | _ -> left
+    let rec loop left =
+      match current state with
+      | Some PLUS ->
+          advance state;
+          let right = parse_mul () in
+          loop (Expr.Add (left, right))
+      | Some MINUS ->
+          advance state;
+          let right = parse_mul () in
+          loop (Expr.Sub (left, right))
+      | _ -> left
+    in
+    loop (parse_mul ())
   and parse_mul () =
-    let left = parse_primary () in
-    match current state with
-    | Some TIMES ->
-        advance state;
-        let right = parse_mul () in
-        Expr.Mul (left, right)
-    | Some DIV ->
-        advance state;
-        let right = parse_mul () in
-        Expr.Div (left, right)
-    | _ -> left
+    let rec loop left =
+      match current state with
+      | Some TIMES ->
+          advance state;
+          let right = parse_primary () in
+          loop (Expr.Mul (left, right))
+      | Some DIV ->
+          advance state;
+          let right = parse_primary () in
+          loop (Expr.Div (left, right))
+      | _ -> left
+    in
+    loop (parse_primary ())
   and parse_primary () =
     match current state with
     | Some (INT n) ->
